a7367b97af5749d9474b6c0a23aa4e726ba60484
Artur Arede <arturricardo_a@hotmail.com>
2019-08-29 10:04:34 +0100

    added 3 buttons and city temperature functionality

--- server.js
+++ server.js
@@ -1,31 +1,45 @@
-var express = require('express')
-var request = require('request');
+const express = require('express')
+const request = require('request');
+const bodyParser = require('body-parser');
+const app = express();
 
-var app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-var city = 'Lisbon';
-var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=a99f2687b4dfbb59aed4f7bba6d28515`;
+const apiKey = 'a99f2687b4dfbb59aed4f7bba6d28515'
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
+    res.render('weather', {weather: null, error: null});
+  })
+
+app.post('/', function(req, res) {
+    let city = req.body.city_name1;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
     request(url, function(error, response, body){
-        weather_json = JSON.parse(body);
-        console.log(weather_json);
+        if(error){
+            res.render('weather', {weather: null, error: 'Error, please try again'});
+        } else {
+            var weather_json = JSON.parse(body);
 
-        var weather = {
-            city : city,
-            temperature : Math.round(weather_json.main.temp),
-            description : weather_json.weather[0].description, 
-            icon : weather_json.weather[0].icon
-        };
+            if(weather_json.main == undefined){
+                res.render('weather', {weather: null, error: 'Error, please try again'});
+            } else {
+                var weather_data = {
+                    city : city,
+                    temperature : Math.round(weather_json.main.temp),
+                    description : weather_json.weather[0].description, 
+                    icon : weather_json.weather[0].icon
+                };
+                res.render('weather', {weather: weather_data, error: null});
+            }
+        }
 
-        var weather_data = {weather : weather};
-
-        res.render('weather', weather_data);
     });
 
 });
 
-app.listen(8000);
+
+app.listen(8000, function () {
+    console.log('App listening on port 8000!')
+})
