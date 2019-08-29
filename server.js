83b70f711c60a04e6b4477214c2c177e7e259e20
Artur Arede <arturricardo_a@hotmail.com>
2019-08-29 11:08:44 +0100

    weather from 3 cities

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const request = require('request');
+const request = require('request-promise');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -9,36 +9,42 @@ app.set('view engine', 'ejs');
 const apiKey = 'a99f2687b4dfbb59aed4f7bba6d28515'
 
 app.get('/', function (req, res) {
-    res.render('weather', {weather: null, error: null});
+    res.render('weather', {weather_data: null, error: null});
   })
 
 app.post('/', function(req, res) {
-    let city = req.body.city_name1;
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    var cities = [req.body.city_name1, req.body.city_name2, req.body.city_name3];
 
-    request(url, function(error, response, body){
-        if(error){
-            res.render('weather', {weather: null, error: 'Error, please try again'});
-        } else {
-            var weather_json = JSON.parse(body);
+    getWeather(cities).then(function(results) {
 
-            if(weather_json.main == undefined){
-                res.render('weather', {weather: null, error: 'Error, please try again'});
-            } else {
-                var weather_data = {
-                    city : city,
-                    temperature : Math.round(weather_json.main.temp),
-                    description : weather_json.weather[0].description, 
-                    icon : weather_json.weather[0].icon
-                };
-                res.render('weather', {weather: weather_data, error: null});
-            }
-        }
+        res.render('weather', {weather_data: results, error: null});
 
     });
-
 });
 
+async function getWeather(cities) {
+    var weather_data = [];
+
+    for (var city_obj of cities) {
+        var city = city_obj;
+        var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+        var response_body = await request(url);
+
+        var weather_json = JSON.parse(response_body);
+
+        var weather = {
+            city : city,
+            temperature : Math.round(weather_json.main.temp),
+            description : weather_json.weather[0].description,
+            icon : weather_json.weather[0].icon
+        };
+
+        weather_data.push(weather);
+    }
+
+    return weather_data;
+}
 
 app.listen(8000, function () {
     console.log('App listening on port 8000!')
